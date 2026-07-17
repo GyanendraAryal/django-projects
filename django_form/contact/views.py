918b25396863a82adfd0f412db64e58ece477c2c
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-07-17 22:53:34 +0545

    django form implementation

---
+++ django_form/contact/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
+
+
+# Create your views here.
+def contact_form(request):
+    return render(request, "contact_form.html")
+
+def submit_contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        message = request.POST.get("message")
+
+        if name and message:
+            Contact.objects.create(name=name, message=message)
+            return HttpResponse(f"Thank you {name}, for your message!!")
+        else:
+            return HttpResponse("Please provide all the values", status=400)
+    return redirect("contact")
