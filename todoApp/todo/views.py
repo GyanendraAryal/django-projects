57cc44d96ec6726ba63d9e615cf97dfb6dc0a128
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-07-18 22:28:29 +0545

    todo app

---
+++ todoApp/todo/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from .models import Task
+
+
+# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all().order_by("-created_at")
+    return render(request, "todo/task_list.html", {"tasks": tasks})
+
+
+def task_create(request):
+    if request.method == "POST":
+        title = request.POST.get("title", "").strip()
+        description = request.POST.get("description", "").strip()
+        if title:
+            Task.objects.create(title=title, description=description)
+            return redirect(reverse("todo:task_list"))
+        error = "Title cannot be empty"
+        return render(request, "todo/task_form.html",{"error":error})
+    return render(request, "todo/task_form.html")
+
+
+def task_update(request,id):
+    task= get_object_or_404(Task, id= id)
+    if request == "POST":
+        title = request.POST.get('title', '').strip()
+        description = request.POST.get("description", "").strip()
+        completed = request.POST.get('completed')  == 'on'
+
+        if title:
+            title = task.title
+            description = task.description
+            completed = task.completed
+            task.save()
+            return redirect(reverse("todo:task_list"))
+        return render(request,'todo/task_form.html',{"task":task,"error":"Title cannot be empty!"})
+    return render(request,"todo/task_form.html",{"task":task})
+
+
+def task_delete(request,id):
+    task=get_object_or_404(Task,id=id)
+    if request == "POST":
+        task.delete()
+        return redirect(reverse("todo:task_list"))
+    return render(request,"todo/task_confirm_delete.html",{"task":task})
+
+
+def task_toogle(request,id):
+    task=get_object_or_404(Task,id=id)
+    if request.method=="POST":
+        task.completed = not task.completed
+        task.save()
+    return redirect("todo:task_list")
